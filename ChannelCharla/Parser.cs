bd7312a5df72c8058d5786c9d196ec11d40135b5
Iñaki <jipcgr@gmail.com>
2022-10-03 23:56:46 +0200

    Esqueleto del codigo

---
+++ ChannelCharla/Parser.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System.Diagnostics;
+
+namespace ChannelCharla
+{
+    public class Parser : IHostedService
+    {
+        private readonly IConfiguration _configuration;
+        private readonly ILogger<Parser> _log;
+        public Parser(IConfiguration configuration, ILogger<Parser> logger)
+        {
+            _configuration = configuration;
+            _log = logger;            
+        }
+
+        protected Task ExecuteAsync(CancellationToken stoppingToken)
+        {
+            try
+            {
+                _log.LogInformation("Start the process");
+                var sw = new Stopwatch();
+                sw.Start();
+                
+                sw.Stop();
+                _log.LogInformation("Tiempo transcurrido {tiempo}", sw.Elapsed);
+                _log.LogInformation("End the process");
+            }
+            catch (Exception ex)
+            {
+                _log.LogError("Error exception {ex}", ex);
+            }
+
+            return Task.CompletedTask;
+        }
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            return ExecuteAsync(cancellationToken);
+        }
+
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ChannelCharla/Program.cs
@@ -0,0 +1,12 @@
+﻿using ChannelCharla;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = Host.CreateDefaultBuilder(args)
+    .ConfigureServices(services =>
+    {
+        services.AddHostedService<Parser>();
+    })
+    .Build();
+
+await host.StartAsync();
